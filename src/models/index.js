398a9e7181c37ba20b6d360befe71631d0574079
ToquiNovic <toquicaagudelo123@gmail.com>
2025-02-07 10:50:33 -0500

    fix category name

--- src/models/index.js
+++ src/models/index.js
@@ -10,7 +10,7 @@ import TypeDocument from "./typeDocument.js";
 import User from "./user.js";
 import Survey from "./survey.js";
 import Question from "./question.js";
-import Category from "./category.js";
+import Category from "./Category.js";
 import SubCategory from "./subCategory.js";
 import TypeSurveys from "./typeSurveys.js";
 import Role from "./Role.js";
